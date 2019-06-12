7d4dade73d68d1e046a0250d69456fa5d86d1abb
alechp <me@alechp.com>
2019-06-12 12:13:59 -0700

    Moved from jest to ava and confirmed working

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,6 +1,7 @@
+import test from "ava";
 const chalk = require("chalk");
 const path = require("path");
-const { File } = require("alpha-genesis");
+const { File } = require("../index");
 const log = console.log;
 const sandbox = path.join(__dirname, "sandbox");
 const output = path.join(sandbox, "output");
@@ -14,7 +15,7 @@ function fileExists(filepath) {
     return err;
   }
 }
-test("File proxied", () => {
+test("File proxied", t => {
   const gitignore = `
     .DS_Store
     node_modules
@@ -22,17 +23,19 @@ test("File proxied", () => {
   let filePathPlain = path.join(output, ".gitignore");
   File(filePathPlain).plain(gitignore);
   let out = fileExists(filePathPlain);
-  expect(out).toBe(true);
+  t.true(out);
 });
 
-test("Library proxied", () => {
+test("Library proxied", t => {
   const { con } = require(`${sandbox}/library.config.sample.js`);
   const { Library } = require("../index.js");
   Library(con).generate();
+  t.pass();
 });
 
-test("Content.template proxied", () => {
+test("Content.template proxied", t => {
   const { template } = require("../index.js");
   let variables = { component: "Foobar" };
   template(`${sandbox}/templates/sample.template.js`, variables);
+  t.pass();
 });
